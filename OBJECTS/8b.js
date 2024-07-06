d978aabc52c119d68df0172e242f39f84b842432
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-06 11:48:55 +0530

    Create 8b.js

---
+++ OBJECTS/8b.js
@@ -0,0 +1,8 @@
+
+
+            let product={
+                name:'basketball',
+                price:2095
+            };
+            product.price += 5;
+            console.log(product.price);
