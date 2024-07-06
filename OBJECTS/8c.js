1ace086cebae99741ca679affdc983b6562ee120
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-06 11:49:16 +0530

    Create 8c.js

---
+++ OBJECTS/8c.js
@@ -0,0 +1,8 @@
+
+            const product={
+                name:'basketball',
+                price:2095,
+                ['delivery-time']:'3-days'
+            };
+            console.log(product['delivery-time']);
+ 
