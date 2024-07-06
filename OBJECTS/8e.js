2b824c735d48979f2db554296a1a0172454e6974
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-07-06 11:49:53 +0530

    Create 8e.js

---
+++ OBJECTS/8e.js
@@ -0,0 +1,18 @@
+
+            function isSameProduct(p1,p2)
+            {
+                if(p1.name === p2.name && p1.price === p2.price){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            const p1={
+                name:'priya',
+                price:10023
+            };
+            const p2={
+                name:'priya',
+                price:10023
+            };
